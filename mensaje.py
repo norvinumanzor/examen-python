b242fc479439c1ef3538f91bbcb422b3122b6e6d
norvinumanzor <est.nhumanzora0201@uml.edu.ni>
2025-06-21 20:28:59 -0300

    Se agrega el Proyecto

---
+++ mensaje.py
@@ -0,0 +1,31 @@
+import tkinter as tk
+from tkinter import messagebox
+
+def mostrar_mensaje():
+    """Muestra un mensaje de alerta"""
+    messagebox.showinfo("Mensaje","18 años")
+
+def mostrar_mensaje():
+    """Muestra un mensaje de alerta"""
+    messagebox.showinfo("Mensaje","Totogalpa")
+
+root = tk.Tk()
+root.title("Examen primer parcial - Norvin Umanzor")
+
+#Agregar una etiqueta de titulo con el nombre "nombre de usuario"
+etiqueta_usuario = tk.Label(root, text="Mi nombre es Norvin")
+etiqueta_usuario.pack(pady=10)
+
+boton= tk.Button(root, text="Edad", command=mostrar_mensaje)
+boton.pack(side='left', padx=5, pady=10)
+
+boton= tk.Button(root, text="Vivo en", command=mostrar_mensaje)
+boton.pack(side='right', padx=5, pady=10)
+
+# Configurar tamaño de la ventana 
+root.geometry("300x200")
+#EStablecer un color de fondo
+root.configure(bg='lightblue')
+
+ # Ejecutar el bucle principal de la aplicacion
+root.mainloop()
